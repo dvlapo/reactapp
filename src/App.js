be3d711282b1b7281596ea1f0bd559acda841ec0
dvlapo <agbooladolapo24@gmail.com>
2021-04-29 13:40:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from "./images/logo.svg"
+import mockup from "./images/illustration-mockups.svg"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <header>
+      <img src={logo} alt="logo"/>
+    </header>
+
+    <main>
+      <img src={mockup} alt="mockup"/>
+      <div className="content">
+        <h2>Build The Community Your Fans Will Love</h2>
+        <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+  Create connections with your users as you engage</p>
+        <button>Register</button>
+      </div>
+    </main>
+    <footer>
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+    </footer>
+    
+
+    </div>
+  );
+}
+
+export default App;
